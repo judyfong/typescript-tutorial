0cfde3068aabc8c5ad5362d8d91122c3f1baf522
judyfong <judyfong@users.noreply.github.com>
2020-07-16 16:26:20 +0000

    Make a thing

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,4 @@
 "use strict";
 console.log('testing some more');
+console.log('will this ever make a gh-pages branch?');
 console.log('github action added but didn not work testing again');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,3 +1,4 @@
 console.log('testing some more');
+console.log('will this ever make a gh-pages branch?');
 console.log('github action added but didn not work testing again');
 
