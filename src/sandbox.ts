dacb36bf7f422e50dac0934c50090d00a5a3817a
judyfong <judyfong@users.noreply.github.com>
2020-07-16 17:30:47 +0000

    testing if it builds

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -3,4 +3,7 @@ console.log('Im about to give up here');
 console.log('will this ever make a gh-pages branch?');
 console.log('still trying after a decade. I might die soon from exhaustion');
 console.log('github action added but didn not work testing again');
+let num1 = 5;
+let num2 = 6;
+console.log(num1 + num2);
 
