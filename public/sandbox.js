29ffd53f10276edd154437e8aef83b7304404f9a
judyfong <judyfong@users.noreply.github.com>
2020-07-16 16:03:43 +0000

    Remove mention of .gitignore in yml file

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,3 @@
 "use strict";
 console.log('testing some more');
-console.log('testing with prebuilt js files');
 console.log('github action added but didn not work testing again');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,4 +1,3 @@
 console.log('testing some more');
-console.log('testing with prebuilt js files');
 console.log('github action added but didn not work testing again');
 
